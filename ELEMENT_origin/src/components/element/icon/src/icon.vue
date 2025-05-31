c623507dbe5ea7acb5df55392ff58c5dd6e29dd4
guijunqiao <gjq3535139173@qq.com>
2025-05-31 21:59:33 +0800

    icon组件成功制作

---
+++ ELEMENT_origin/src/components/element/icon/src/icon.vue
@@ -0,0 +1,31 @@
+<template>
+  <i :class="['el-icon','el-icon-'+instance.props.name]"></i>
+</template>
+
+
+<script setup lang='js' name="ElIcon">
+import { getCurrentInstance } from "vue";
+
+
+defineProps({
+  name:{
+    type:String
+  }
+})
+
+//获取当前组件实例
+const instance = getCurrentInstance();
+
+console.log("123");
+
+
+</script>
+
+
+
+<style>
+
+
+
+
+</style>
--- ELEMENT_origin/src/main.ts
+++ ELEMENT_origin/src/main.ts
@@ -1,10 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import "./assets/theme-chalk/aside.scss";
-import "./assets/theme-chalk/header.scss";
-import "./assets/theme-chalk/footer.scss";
-import "./assets/theme-chalk/container.scss";
-import "./assets/theme-chalk/main.scss";
+import "./assets/theme-chalk/index.scss";
 
 
 createApp(App).mount('#app')
