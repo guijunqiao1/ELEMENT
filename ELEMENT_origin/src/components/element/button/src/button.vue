44da896b88ad798ca4cb39b078ebf968fdf5d989
guijunqiao <gjq3535139173@qq.com>
2025-06-01 14:33:43 +0800

    完成默认按钮的编写

--- ELEMENT_origin/src/components/element/button/src/button.vue
+++ ELEMENT_origin/src/components/element/button/src/button.vue
@@ -1,30 +1,21 @@
 <template>
-  <button>
+  <button :class="['el-button',type? `el-button--${type}`:'']">
     <slot></slot>
   </button>
 </template>
 
+<!-- 使用正则则class数组的书写元素中直接书写正则即可，而元素为对象的书写形式仅针对特殊语法--可以参考先前的复制类名的方式 -->
 
 <script setup lang='js' name="ElButton">
-import { getCurrentInstance } from "vue";
 
-
-defineProps({
-
-})
-
-//获取当前组件实例
-const instance = getCurrentInstance();
-
-
+  defineProps({
+    type:{
+      type:String
+    }
+  })
 
 </script>
 
 
 
-<style>
-
-
-
-
-</style>
+<style></style>
---
+++ ELEMENT_origin/src/components/element/header/src/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="el-header" :style="{ height: instance.props.height }">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="js" setup name="ElHeader">
+import {getCurrentInstance} from "vue";
+
+defineProps({//生成代理，为template模板中的匹配上的属性自动添加上instance.props的前置索引--会提前检查是否有了这个索引
+  height: {
+    default: "60px",
+    type: String
+  }
+})
+
+const instance = getCurrentInstance();
+// console.log("instance_really:"+instance.props.height===height);
+
+</script>
+
+<style lang="scss"></style>
