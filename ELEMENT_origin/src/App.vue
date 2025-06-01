9b77cb7baa74d2aaa1c13a31dd36437b7c45a652
guijunqiao <gjq3535139173@qq.com>
2025-06-01 19:44:27 +0800

    按钮组完成编写

--- ELEMENT_origin/src/App.vue
+++ ELEMENT_origin/src/App.vue
@@ -1,40 +1,23 @@
 <template>
   <div id="app">
-    <!-- 文字按钮 -->
-    <el-button type="text">文字按钮</el-button>
-    <el-button type="text" disabled>文字按钮</el-button>
-    <el-button type="primary" icon="el-icon-edit"></el-button>
-    <el-button type="primary" icon="el-icon-share"></el-button>
-    <el-button type="primary" icon="el-icon-delete"></el-button>
-    <el-button type="primary" icon="el-icon-search">搜索</el-button>
-    <el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
-    <el-button type="primary"><i class="el-icon-upload el-icon--left"></i>上传</el-button>
-    <el-button type="primary" :loading="true">加载中</el-button>
-
-    <br>
-    <hr>
-      <el-button>默认按钮</el-button>
-      <el-button size="medium">中等按钮</el-button>
-      <el-button size="small">小型按钮</el-button>
-      <el-button size="mini">超小按钮</el-button>
-
-    <br>
-    <hr>
-      <el-button round>默认按钮</el-button>
-      <el-button size="medium" round>中等按钮</el-button>
-      <el-button size="small" round>小型按钮</el-button>
-      <el-button size="mini" round>超小按钮</el-button>
-
-    <br>
-    <hr>
-      <el-button type="primary" icon="el-icon-edit" circle ></el-button>
-      <el-button type="success" icon="el-icon-check" circle size="medium"></el-button>
-      <el-button type="info" icon="el-icon-message" circle size="small"></el-button>
-      <el-button type="warning" icon="el-icon-star-off" circle size="mini"></el-button>
-
-    <br>
-    <hr>
-
+    <el-button-group>
+      <el-button type="primary" icon="el-icon-arrow-left">上一页</el-button>
+      <el-button type="primary">下一页<i class="el-icon-arrow-right el-icon--right"></i></el-button>
+    </el-button-group>
+    <el-button-group>
+      <el-button type="primary" icon="el-icon-edit"></el-button>
+      <el-button type="primary" icon="el-icon-share"></el-button>
+      <el-button type="primary" icon="el-icon-delete"></el-button>
+    </el-button-group>
+        <el-button-group>
+      <el-button type="primary" icon="el-icon-delete"></el-button>
+    </el-button-group>
+    <el-button-group>
+      <el-button round type="primary" icon="el-icon-delete"></el-button>
+    </el-button-group>
+     <el-button-group>
+      <el-button circle type="primary" icon="el-icon-delete"></el-button>
+    </el-button-group>
   </div>
 </template>
 
@@ -43,6 +26,7 @@
 <!-- 补充：上述需要注意的是在组件标签整体中的内容才会被放置在slot中 -->
 
 <script setup lang="js" name="app">
+import ElButtonGroup from "./components/element/button-group/src/button-group.vue";
 import ElButton from "./components/element/button/src/button.vue";
 
 
---
+++ ELEMENT_origin/src/components/element/button-group/src/button-group.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="el-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+
+<script setup lang='js' name="ElButtonGroup">
+
+
+</script>
+
+
+
+<style></style>
