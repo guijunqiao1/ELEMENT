1933cd93964c5df0d8c986f281502530f42b8a47
guijunqiao <gjq3535139173@qq.com>
2025-06-01 21:26:49 +0800

    link组件编写准备工作完成

--- ELEMENT_origin/src/App.vue
+++ ELEMENT_origin/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <el-button autofocus type="primary">按钮</el-button>
-    <el-button @click="handleClick" native-type="submit" type="primary">按钮</el-button>
+    <el-link>主要链接</el-link>
   </div>
 </template>
 
@@ -10,16 +9,8 @@
 <!-- 补充：上述需要注意的是在组件标签整体中的内容才会被放置在slot中 -->
 
 <script setup lang="js" name="app">
-import ElButton from "./components/element/button/src/button.vue";
+import ElLink from "./components/element/link/src/link.vue";
 
-//若子组件没有使用emit发射事件则仅定义普通按钮点击事件,直接点击不会触发事件，因为这是向组件整体进行绑定，但是若将@click修改为@click.native则能够监听到组价整体的盒子
-//绑定上的事件了
-function handleClick(e){
-  console.log(e);
-  // 需要注意此处和子组件中的emit发射过来的函数内容进行配合的流程为：
-  //首先子组件中的根组件和当前组件绑定上了同一个事件，则在子组件中使用了emit进行内容的发射的内容可以在父组件中使用上,则此处成功获取到
-  //传输流名称为clik的参数内容即value(也就是子组件中的$event)
-}
 
 </script>
 
---
+++ ELEMENT_origin/src/components/element/link/src/link.vue
@@ -0,0 +1,12 @@
+<template>
+  <a class="el-link">
+    <slot></slot>
+  </a>
+</template>
+
+
+<script setup lang="js" name="ElLink">
+
+</script>
+
+<style lang="scss"></style>
