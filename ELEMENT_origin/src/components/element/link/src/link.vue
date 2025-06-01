72b52ac1a8ae3c12617626dd32a404deffbfc0ca
guijunqiao <gjq3535139173@qq.com>
2025-06-01 21:43:24 +0800

    完成Link 组件功能编写--后续完成所有的组件的test

--- ELEMENT_origin/src/components/element/link/src/link.vue
+++ ELEMENT_origin/src/components/element/link/src/link.vue
@@ -1,11 +1,40 @@
 <template>
-  <a class="el-link">
-    <slot></slot>
+  <a
+    :class="['el-link',type ? `el-link--${type}` : '',(underline && !disabled) ? 'is-underline': '',disabled ?'is-disabled' : '' ,]"
+    :href="href"
+  >
+    <i v-if="icon" :class="icon"></i>
+    <span v-if="instance.slots.default">
+      <slot></slot>
+    </span>
   </a>
 </template>
 
 
 <script setup lang="js" name="ElLink">
+import {getCurrentInstance} from "vue";
+
+defineProps({
+  type:{
+    type:String
+  },
+  disabled:{
+    type:Boolean
+  },
+  underline:{
+    type:Boolean
+  },
+  href:{
+    type:String
+  },
+  icon:{
+    type:String
+  }
+})
+
+//获取当前组件实例
+const instance = getCurrentInstance();
+
 
 </script>
 
